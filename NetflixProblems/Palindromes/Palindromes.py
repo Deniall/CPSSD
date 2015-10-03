113d1f1c87fd37bd1d794861c3d07a202fe232a6
Niall Walsh <nialternate@gmail.com>
2015-10-03 20:10:22 +0100

    Update Palindromes.py

--- NetflixProblems/Palindromes/Palindromes.py
+++ NetflixProblems/Palindromes/Palindromes.py
@@ -1,12 +1,30 @@
 def iterativePalindrome():
-    palindrome = raw_input().lower()
+
+    palindrome = raw_input().lower() # .lower() makes it case insensitive
+
     length = len(palindrome)
+
     i = 0
+
     while i < length / 2 + 1:
         if palindrome[i] != palindrome[-i - 1]:
-            print "F"
-            iterativePalindrome()
+            return False # iterates until it finds a case where two corresponding letters are not the same
+
         i += 1
+
     else:
-        print "T"
-        iterativePalindrome()
+        return True
+
+
+def recursivePalindrome(palindrome):
+
+    palindrome.lower() # makes the function case insensitive
+
+    if len(palindrome) < 2:
+        return True # the base case
+    if palindrome[0] != palindrome[-1]:
+        return False #this means the first and last letters are different, hence not a palindrome
+
+    return recursivePalindrome(palindrome[1:-1]) # the recursive step
+        
+            
