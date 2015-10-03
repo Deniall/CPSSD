4665f840c5fa17b3c8e37c2242e6f857dff43b01
Niall Walsh <nialternate@gmail.com>
2015-10-03 18:10:34 +0100

    Update Palindromes.py

--- NetflixProblems/Palindromes/Palindromes.py
+++ NetflixProblems/Palindromes/Palindromes.py
@@ -1,6 +1,17 @@
-palindrome = raw_input()
-first_half, second_half = palindrome[:len(palindrome)//2], palindrome[len(palindrome)//2:]
-if second_half[::-1] == first_half:
-    print "True"
-else:
-    print "False"
+def palindromeIterative(i=0):
+    if i == 0:
+        print "This is the iterative version of the program that checks if a string is a palindrome. To break out of the function, type 'exit'."
+    palindrome = raw_input().lower()
+    first_half = palindrome[:len(palindrome)//2]
+    second_half = palindrome[len(palindrome)//2:]
+    if len(palindrome)%2 != 0:
+        first_half = palindrome[:len(palindrome)//2+1]
+    if palindrome == "exit":
+        a = 1 / 0  # will raise an exception, this is to break out of the function
+        return a
+    if second_half[::-1] == first_half:
+        print "True"
+        palindromeIterative(i+1)
+    else:
+        print "False"
+        palindromeIterative(i+1)
