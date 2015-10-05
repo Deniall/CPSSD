080cdb37f4966e5e48a74575f4406b46c4e4ef8d
Niall Walsh <nialternate@gmail.com>
2015-10-05 03:07:07 +0100

    Update Palindromes.py

--- NetflixProblems/Palindromes/Palindromes.py
+++ NetflixProblems/Palindromes/Palindromes.py
@@ -26,5 +26,22 @@ def recursivePalindrome(palindrome):
         return False #this means the first and last letters are different, hence not a palindrome
 
     return recursivePalindrome(palindrome[1:-1]) # the recursive step
-        
-            
+
+def anagramPalindrome():
+    from itertools import permutations # this module finds all permuations of a string
+    string = raw_input().lower()
+    anagrams = [''.join(chars) for chars in permutations(string)] # creates a set of the permuations(anagranms)
+    i = 0
+    k = 0
+    while i <= len(set(anagrams)): # the earlier iterative function to check palindromes(badly implemented)
+        palindrome = anagrams[i] 
+        length = len(palindrome)
+        while k < length / 2 + 1:
+            if palindrome[k] != palindrome[-k - 1]:
+                return "False" 
+
+                k += 1
+            else:
+                return "True"
+        i+=1
+    
